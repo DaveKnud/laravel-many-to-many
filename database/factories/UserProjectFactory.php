efca8b61dc286885519ab1ca645898e37d78e87d
DaveKnud <daveknud@outlook.com>
2023-07-26 16:22:33 +0200

    pushato

--- database/factories/UserProjectFactory.php
+++ database/factories/UserProjectFactory.php
@@ -11,14 +11,14 @@ class UserProjectFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
+     *phpartisan migrate:refresh --sed
      * @return array<string, mixed>
      */
     public function definition()
     {
         return [
             'name' => fake()->words(10),
-            'description' => fake()->word()
+            'description' => fake()->paragraph(100)
         ];
     }
 }
